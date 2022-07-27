36e5894079464f9d6d5773b4cf6f57281f460ee5
ridwanfbnr <ridwanfebnur9@gmail.com>
2022-07-27 19:02:23 +0700

    navigating

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,9 @@
+import { useNavigate } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetup() {
+    const navigate = useNavigate();
+
     function addMeetupHandler(newMeetupData) {
         fetch(
             "https://react-getting-started-7fbb1-default-rtdb.firebaseio.com/meetups.json",
@@ -11,7 +14,9 @@ function NewMeetup() {
                     "Content-Type": "application/json",
                 }
             }
-        )
+        ).then(() => {
+            navigate("/");
+        });
     }
 
     return (
