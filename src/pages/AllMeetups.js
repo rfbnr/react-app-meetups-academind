896b1b9c97076d8829c6d4124e7c0c3c138c779c
ridwanfbnr <ridwanfebnur9@gmail.com>
2022-07-27 15:06:54 +0700

    adding more components

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,3 +1,5 @@
+import MeetupList from "../components/meetups/MeetupList";
+
 const DUMMY_DATA = [
     {
         id: "m1",
@@ -21,13 +23,7 @@ function AllMeetups() {
     return (
         <section>
             <h1>All Meetups Page</h1>
-            <ul>
-                {DUMMY_DATA.map((meetup) => {
-                    return (
-                        <li key={meetup.id}>{meetup.title}</li>
-                    )
-                })}
-            </ul>
+            <MeetupList meetups={DUMMY_DATA}/>
         </section>
     );
 }
