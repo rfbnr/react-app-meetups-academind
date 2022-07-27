233009e5e411a81e16e18e97c7c7f67a11871ba6
ridwanfbnr <ridwanfebnur9@gmail.com>
2022-07-27 18:45:24 +0700

    sending a post request

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,23 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetup() {
+    function addMeetupHandler(newMeetupData) {
+        fetch(
+            "https://react-getting-started-7fbb1-default-rtdb.firebaseio.com/meetups.json",
+            {
+                method: "POST",
+                body: JSON.stringify(newMeetupData),
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            }
+        )
+    }
+
     return (
         <section>
             <h1>Add New Meetup</h1>
-            <NewMeetupForm/>
+            <NewMeetupForm onAddMeetup={addMeetupHandler}/>
         </section>
     )
 }
