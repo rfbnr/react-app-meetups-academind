17dad4c1673d6b3c4599e919d3377468e7991f49
ridwanfbnr <ridwanfebnur9@gmail.com>
2022-07-27 13:56:37 +0700

    adding links & navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import { Routes, Route } from "react-router-dom";
+import MainNavigation from "./components/layout/MainNavigation";
 import AllMeetups from "./pages/AllMeetups";
 import Favorites from "./pages/Favorites";
 import NewMeetup from "./pages/NewMeetup";
@@ -7,6 +8,7 @@ import NewMeetup from "./pages/NewMeetup";
 function App() {
   return (
     <div>
+      <MainNavigation/>
       <Routes>
         <Route path="/" element={<AllMeetups/>}/>
         <Route path="new-meetup" element={<NewMeetup/>}/>
