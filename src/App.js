535d61386db88dc788ee7048df1178c2e0591979
ridwanfbnr <ridwanfebnur9@gmail.com>
2022-07-27 15:42:36 +0700

    building wrapper components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 
 import { Routes, Route } from "react-router-dom";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 import AllMeetups from "./pages/AllMeetups";
 import Favorites from "./pages/Favorites";
 import NewMeetup from "./pages/NewMeetup";
 
 function App() {
   return (
-    <div>
-      <MainNavigation/>
+    <Layout>
       <Routes>
         <Route path="/" element={<AllMeetups/>}/>
         <Route path="new-meetup" element={<NewMeetup/>}/>
         <Route path="favorites" element={<Favorites/>}/>
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
